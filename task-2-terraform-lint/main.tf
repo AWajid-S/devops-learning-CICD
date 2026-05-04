0dfd9073f17607b44852265798414c431ddc01f7
AWajid-S <wajidsalam7@gmail.com>
2026-05-04 11:59:10 +0100

    Add Terraform formatting lint check

--- task-2-terraform-lint/main.tf
+++ task-2-terraform-lint/main.tf
@@ -1,12 +1,3 @@
-provider "aws" {
-  region = "us-east-1
-}
+provider "aws"{region="us-east-1"}
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-terraform-lint-bucket-123456"
-
-  tags = {
-    Name        = "MyBucket"
-    Environment = "Dev"
-  }
-}
+resource "aws_s3_bucket" "example"{bucket="my-terraform-lint-bucket-123456"tags={Name="MyBucket"Environment="Dev"}}
