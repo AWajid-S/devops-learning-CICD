7d4a26fe94bb3324d4241032b9c12eb17fa02364
AWajid-S <wajidsalam7@gmail.com>
2026-05-04 12:00:52 +0100

    Fix syntax but keep formatting issues

--- task-2-terraform-lint/main.tf
+++ task-2-terraform-lint/main.tf
@@ -1,3 +1,11 @@
-provider "aws"{region="us-east-1"}
+provider "aws" {
+region = "us-east-1"
+}
 
-resource "aws_s3_bucket" "example"{bucket="my-terraform-lint-bucket-123456"tags={Name="MyBucket"Environment="Dev"}}
+resource "aws_s3_bucket" "example" {
+bucket = "my-terraform-lint-bucket-123456"
+tags = {
+Name = "MyBucket"
+Environment = "Dev"
+}
+}
