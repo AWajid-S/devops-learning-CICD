d566d74caab8c4a11d3f98fc226934c48e050924
AWajid-S <wajidsalam7@gmail.com>
2026-05-04 11:49:53 +0100

    Add terraform lint pipeline with an error

---
+++ task-2-terraform-lint/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-terraform-lint-bucket-123456"
+
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Dev"
+  }
+}
